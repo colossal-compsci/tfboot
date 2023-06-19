9e2657ba670a34a6585ff5971b952a02b7681ef4
Stephen Turner <vustephen@gmail.com>
2023-06-19 06:24:18 -0400

    successfully construct a chicken VCF

--- inst/extdata/scratch.R
+++ inst/extdata/scratch.R
@@ -1,6 +1,5 @@
 library(BSgenome.Ggallus.UCSC.galGal6)
 library(TxDb.Ggallus.UCSC.galGal6.refGene)
-library(org.Gg.eg.db)
 library(VariantAnnotation)
 
 makesnp <- function(x) {
@@ -18,15 +17,17 @@ bsgenome <- BSgenome.Ggallus.UCSC.galGal6
 chromosome_lengths <- seqlengths(bsgenome)
 
 # Define how many SNPs you want to generate per chromosome
-num_snps <- 100  # Change as needed
+num_snps <- 20000  # Change as needed
 
 # Initialize an empty GRanges object to store your simulated SNPs
 simulated_snps <- GRanges()
 
-chr_name <- "chr33"
 # Loop through each chromosome
-# for (chr_name in names(chromosome_lengths)) {
-for (chr_name in "chr33") {
+for (chr_name in names(chromosome_lengths)) {
+
+  # skip everything except chr33
+  if (chr_name!="chr33") next
+
   # Generate random positions for the SNPs
   snp_positions <- sort(sample(1:chromosome_lengths[chr_name], num_snps))
 
@@ -35,28 +36,29 @@ for (chr_name in "chr33") {
                       ranges = IRanges(start = snp_positions, width = 1),
                       strand = "*")
 
-  chr_snps$REF <- getSeq(bsgenome,
-                         names = rep(chr_name, length(snp_positions)),
-                         start = snp_positions,
-                         end = snp_positions)
-
-  chr_snps$ALT <-
-    chr_snps$REF |>
-    unlist() |>
-    as.character() |>
-    strsplit(split="") |>
-    unlist() |>
-    sapply(makesnp) |>
-    setNames(rep(chr_name, length(snp_positions))) |>
-    DNAStringSet()
-
   # Add the simulated SNPs for this chromosome to the total
   simulated_snps <- c(simulated_snps, chr_snps)
 }
 
-vcf <- VCF(rowRanges = simulated_snps)
-ref(vcf) <- simulated_snps$REF
-alt(vcf) <- simulated_snps$ALT |> as.character() |> VariantAnnotation:::.toDNAStringSetList()
+# Make the VCF
+vcf <- VCF(rowRanges = simulated_snps, colData=DataFrame(Samples=1L, row.names = "sample1"))
 
-writeVcf(vcf, here::here("inst/extdata/galGal6-chr38.vcf"))
-system(paste("head -n10", here::here("inst/extdata/galGal6-chr38.vcf")))
+# Set ref alleles (DNAStringSet)
+ref(vcf) <- getSeq(bsgenome, simulated_snps)
+
+# Set ref alleles (DNAStringSetList)
+alt(vcf) <-
+  ref(vcf) |>
+  as.character() |>
+  sapply(makesnp) |>
+  unname() |>
+  VariantAnnotation:::.toDNAStringSetList()
+
+# Set the genotypes
+assays(vcf, withDimnames=FALSE) <- list(GT=matrix(rep("1/1", length(vcf)), ncol=1, dimnames = list(NULL, "sample1")))
+
+# Write the VCF and look at a few
+vcffile <- here::here("inst/extdata/galGal6-chr38.vcf")
+writeVcf(vcf, vcffile)
+system(paste("head -n10", vcffile))
+system(paste("bgzip", vcffile))
