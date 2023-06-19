a94455d1b27f1c37e4dc206d23f272cc5e831bad
Stephen Turner <sturner@signaturescience.com>
2023-06-19 05:40:48 -0400

    progress on VCF

--- inst/extdata/scratch.R
+++ inst/extdata/scratch.R
@@ -23,21 +23,22 @@ num_snps <- 100  # Change as needed
 # Initialize an empty GRanges object to store your simulated SNPs
 simulated_snps <- GRanges()
 
+chr_name <- "chr33"
 # Loop through each chromosome
 # for (chr_name in names(chromosome_lengths)) {
 for (chr_name in "chr33") {
   # Generate random positions for the SNPs
   snp_positions <- sort(sample(1:chromosome_lengths[chr_name], num_snps))
 
-  # Generate random nucleotide substitutions
-  snp_substitutions <- sample(c("A", "T", "C", "G"), num_snps, replace = TRUE)
-
   # Generate GRanges object for this chromosome
   chr_snps <- GRanges(seqnames = chr_name,
                       ranges = IRanges(start = snp_positions, width = 1),
                       strand = "*")
 
-  chr_snps$REF <- getSeq(bsgenome, names = rep(chr_name, length(snp_positions)), start = snp_positions, end = snp_positions)
+  chr_snps$REF <- getSeq(bsgenome,
+                         names = rep(chr_name, length(snp_positions)),
+                         start = snp_positions,
+                         end = snp_positions)
 
   chr_snps$ALT <-
     chr_snps$REF |>
@@ -54,23 +55,8 @@ for (chr_name in "chr33") {
 }
 
 vcf <- VCF(rowRanges = simulated_snps)
+ref(vcf) <- simulated_snps$REF
+alt(vcf) <- simulated_snps$ALT |> as.character() |> VariantAnnotation:::.toDNAStringSetList()
 
-writeVcf(vcf, here::here("inst/extdata/galGal6-chr38.vcf.gz"))
-
-
-
-# Define the new sample name
-new_sample <- "sample1"
-
-# Create a new DataFrame for the new sample
-new_sample_df <- DataFrame(sampleID = new_sample)
-
-# Add the new sample to colData(vcf)
-colData(vcf) <- new_sample_df
-
-# Check if the new sample has been added
-colData(vcf)
-
-
-gt <- matrix(sample(c("0/1", "1/1"), size=length(vcf), replace=TRUE))
-geno(vcf)$GT <- gt
+writeVcf(vcf, here::here("inst/extdata/galGal6-chr38.vcf"))
+system(paste("head -n10", here::here("inst/extdata/galGal6-chr38.vcf")))
